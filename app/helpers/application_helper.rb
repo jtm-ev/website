75ba57e0ac71684692d114f97c2d25440f502db3
Florian Günther <mail@gee-f.de>
2013-02-05 14:28:57 +0100

    visually highlight active navigation link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def navigation_link_to(text, href)
+    tag_class = (href == request.path) ? 'active' : ''
+    content_tag :a, href: href, class: tag_class do
+      text
+    end
+  end
 end
