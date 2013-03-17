6141ee6312893f29b46044a450d7818ce6b7d5a7
Florian Günther <mail@gee-f.de>
2013-03-17 14:02:09 +0100

    transparente bilder für gif, png, pdf. closes #32

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,7 @@
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :users, :join_table => :users_roles
   has_and_belongs_to_many :members, :join_table => :members_roles
+  
   belongs_to :resource, :polymorphic => true
   
   scopify
--- lib/file_upload.rb
+++ lib/file_upload.rb
@@ -12,40 +12,15 @@ module FileUpload
     # Auto-Rotate gibt bei manchen Bildern Probleme (EXIF autoorient?): http://pjkh.com/articles/rails-paperclip-auto-orient-and-resizing/
     # https://github.com/thoughtbot/paperclip/wiki/Interpolations
     has_attached_file :file, {
-      # default_options: {
-      #   auto_orient: false
-      # },
-      # auto_orient: false,
-      # styles: {
-      #   square: {geometry: '100x100#', format: :png}, #, auto_orient: true, orient: 'top-left'
-      #   # thumb:  {geometry: '120x120', format: :png},
-      #   # medium: {geometry: '540x540', format: :png},
-      #   # large:  {geometry: '1024x1024', format: :png},
-      #   # large_cinema: {geometry: '1024x576^', format: :png}  #16:9
-      #   large_cinema: lambda { |instance|
-      #     if instance.landscape?
-      #       {
-      #         geometry: '1024x576#', format: :png
-      #       }
-      #     else
-      #       {
-      #         geometry: '1024x', format: :png, convert_options: '+repage -crop 1024x576+0+0 -gravity North'
-      #       }
-      #     end
-      #   }
-      #   # large_photo: {geometry: '1024x768#', format: :png}    #4:3
-      # },
       styles: lambda { |asset|
-        image_format = :jpg
+        image_format = asset.instance.transparent? ? :png : :jpg
         lc_style = { geometry: '1024x576#', format: image_format }
-        # unless asset.instance.landscape?
-        #   lc_style = { geometry: '1024x', format: :png, convert_options: '+repage -crop 1024x576+0+100 -gravity North' }
-        # end
+
         {
-          square: {geometry: '150x150#', format: image_format},
-          square_300: {geometry: '300x300#', format: image_format},
-          normal: {geometry: '350x', format: image_format},
-          large:  {geometry: '1024x', format: image_format},
+          square:     {geometry: '150x150#',  format: image_format},
+          square_300: {geometry: '300x300#',  format: image_format},
+          normal:     {geometry: '350x',      format: image_format},
+          large:      {geometry: '1024x',     format: image_format},
           large_cinema: lc_style
         }
       },
@@ -53,10 +28,11 @@ module FileUpload
         # all: '-auto-orient '
         # all: '-orient top-left'
         # large_cinema: '-gravity North'
-      #   # square: "-quality 75 -strip",
-      #   # thumb:  "-quality 75 -strip",
-      #   # large:  "-quality 100 -strip"  
+        # square: "-quality 75 -strip",
+        # thumb:  "-quality 75 -strip",
+        # large:  "-quality 100 -strip"  
       },
+      
       # url: "/system/project_files/:hash.:extension",
       hash_secret: "207ff786710bf9b55b4481b393cff8be9a64a96088cface4b4ab7cf648cd01d8222030def29bbc52b284e4327c73b97d4e45468e676"
     }
@@ -69,6 +45,10 @@ module FileUpload
   
   def has_image?
     !self.file_file_name.nil?
+  end
+  
+  def transparent?
+    file_file_name =~ %r{\.(gif|png|pdf)$}
   end
   
   def image?
