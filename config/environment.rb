97360ca0987b863f3b78d4f9b0f90aee6bb0f9e5
Florian Günther <mail@gee-f.de>
2013-11-21 09:06:47 +0100

    negative captcha for guestbook

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+NEGATIVE_CAPTCHA_SECRET = "c29ba3d18d36d035559de55b656f069c0c8a4b6a389c57387ac821b1b253de1bc301773410fbd91f29e9df3f3f0fceabee46f3fdd48683652f354b7ac7d9e84c"
+
 # Initialize the rails application
 Website::Application.initialize!
