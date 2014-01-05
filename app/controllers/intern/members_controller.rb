42d3409dc68dc266904cad4c03a78038441e030e
Florian Günther <mail@gee-f.de>
2014-01-05 12:56:23 +0100

    mitglieder können austreten und wieder eintreten

--- app/controllers/intern/members_controller.rb
+++ app/controllers/intern/members_controller.rb
@@ -2,12 +2,12 @@ class Intern::MembersController < ApplicationController
   load_and_authorize_resource except: [:index, :addresses]
   skip_authorization_check only: [:addresses]
   before_filter :authenticate_user!, except: [:show]
-  
+
   add_breadcrumb 'Home', :root_path
-  
+
   def addresses
     add_breadcrumb 'Adressen', address_members_path
-    
+
     if params[:id]
       @group = Group.find(params[:id])
       @members = @group.members
@@ -15,9 +15,9 @@ class Intern::MembersController < ApplicationController
     else
       @members = scope
     end
-    
+
   end
-  
+
   #############################################################################################
   # Mitgliederverwaltung und Owner
   #############################################################################################
@@ -33,14 +33,14 @@ class Intern::MembersController < ApplicationController
         tms.member = target_member
         tms.save
       end
-      
+
       if @member.name != target_member.name
         target_member.birth_name = @member.name
         target_member.save
       end
-      
+
       @member.delete
-      
+
       redirect_to action: :index
       return
     end
@@ -51,10 +51,10 @@ class Intern::MembersController < ApplicationController
 
     respond_to do |format|
       if @member.update_attributes(params[:member])
-        
+
         process_group_memberships
-        
-        
+
+
         format.html { redirect_to :back, notice: 'Mitgliedsdaten erfolgreich gespeichert.' }
         format.json { head :no_content }
       else
@@ -63,19 +63,19 @@ class Intern::MembersController < ApplicationController
       end
     end
   end
-  
+
   #############################################################################################
   # Mitgliederverwaltung
   #############################################################################################
-  
+
   # GET /members
   # GET /members.json
   def index
     authorize! :manage, Member
-    
+
     s = sort_column.split(',').map {|s| "#{s} #{sort_direction}"}
     @members = scope.order(s) #.page(params[:all_page]).per(25)
-    
+
     # scope = scope.order(:name, :first_name)
 
     respond_to do |format|
@@ -83,12 +83,12 @@ class Intern::MembersController < ApplicationController
       format.json { render json: @members }
     end
   end
-  
+
   # GET /members/1/edit
   def edit
     authorize! :manage, Member  # Owner can edit his data using /profile
   end
-  
+
   # GET /members/new
   # GET /members/new.json
   def new
@@ -103,9 +103,9 @@ class Intern::MembersController < ApplicationController
   def create
     respond_to do |format|
       if @member.save
-        
+
         process_group_memberships
-        
+
         format.html { redirect_to action: :index, notice: 'Member was successfully created.' }
         format.json { render json: @member, status: :created, location: @member }
       else
@@ -125,7 +125,17 @@ class Intern::MembersController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
+
+  def deactivate
+    @member.deactivate
+    redirect_to :back
+  end
+
+  def activate
+    @member.activate
+    redirect_to :back
+  end
+
   private
     def scope
       if params[:flags] == 'ehemalige'
@@ -135,17 +145,17 @@ class Intern::MembersController < ApplicationController
       end
       Member.scoped.active
     end
-    
+
     def default_sort_column
       "name"
     end
-    
+
     def process_group_memberships
       del = params[:delete_group_membership]
       del and del.each do |id, value|
         @member.group_memberships.destroy(id.to_i)
       end
-      
+
       gm = params[:new_group_membership]
       if gm and not gm['group_id'].blank?
         @member.group_memberships.create(gm)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Constraint to catch old website links with ?nav= in it to redirect permanently
 module OldWebsiteConstraint
   extend self
-  
+
   def matches?(request)
     request.query_parameters.has_key?('nav')
   end
@@ -20,32 +20,40 @@ Website::Application.routes.draw do
     get   '/login' => 'devise/sessions#new'
     get   '/logout' => 'devise/sessions#destroy'
   end
-  
+
   ############################################################################
   # Intern
   ############################################################################
+  get '/dashboard' => redirect('/i/dashboard')
+
   get '/email' => 'emails#new', as: :email
   post '/email' => 'emails#create'
-  
-  namespace :intern, path: 'i' do    
+
+  namespace :intern, path: 'i' do
     ### User
-    resources :users
-    
+    resources :users do
+
+    end
+
     ### Profile/Dashboard
     match '/profile' => 'users#profile'
     match '/profile/role/:id' => 'users#profile_role', as: :profile_role
     match '/dashboard' => 'users#dashboard', as: :dashboard
-    
-    
+
+
     ### Mitglieder
     resources :members, path: 'mitglieder' do
       collection do
         get 'adressen' => 'members#addresses', as: :address
       end
+      member do
+        get 'deactivate' => 'members#deactivate'
+        get 'activate' => 'members#activate'
+      end
     end
     get '/mitglieder(::flags)' => 'members#index', as: :flagged_members
-    
-    
+
+
     ### Gruppen
     resources :groups, path: 'gruppen' do
       member do
@@ -53,7 +61,7 @@ Website::Application.routes.draw do
       end
     end
     resources :group_memberships
-    
+
     ### Projekte
     resources :projects, path: 'projekte' do
       resources :project_files do
@@ -64,28 +72,28 @@ Website::Application.routes.draw do
 
       resources :teams
     end
-    
+
   end
-  
-  
+
+
   ############################################################################
   # Extern
   ############################################################################
   resources :guestbooks, path: 'gaestebuch'
   post '/gaestebuch/manage' => 'guestbooks#manage'
-  
+
   resources :members, path: 'mitglieder', only: [:show]   # public member profiles
-  
+
   resources :projects, path: 'projekte', only: [:index, :show]
   get '/projekte(::tags)' => 'projects#index', as: :tagged_projects
   get '/projekte(::tags)/:id' => 'projects#show', as: :show_tagged_project
-  
+
   resources :locations, path: 'spielorte' do
     collection do
       put '/' => 'locations#manage'
     end
   end
-  
+
   resources :pages, path: 's', constraints: { id: /[0-9]*/ } do
     resources :page_files
     member do
@@ -107,7 +115,7 @@ Website::Application.routes.draw do
   resources :team_memberships
 
   resources :events
-  
+
 
   ### Image Cropper
   get '/tools/:action' => 'tools'
@@ -159,17 +167,17 @@ Website::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  
+
   if Rails.env.development?
     mount MailPreview => 'mail_view'
   end
-  
+
   #########################################################################
   # Handling old Website Links
   #########################################################################
   match "/" => 'old_website#redirect', constraints: OldWebsiteConstraint
   match "/phpThumb/*path" => 'old_website#redirect_image'
-  
+
   get "/sitemap.:format" => 'home#sitemap'
   root :to => 'home#index'
 
