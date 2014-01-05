bbf6ac6272c216862432b81dcf3a92d400d07aa3
Florian Günther <mail@gee-f.de>
2014-01-05 15:09:44 +0100

    project manager can handle events

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,21 +3,21 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    
+
     user ||= User.new # guest user (not logged in)
     user.member ||= Member.new
-    
-    
+
+
     if user.has_role?(:admin)
       can :manage, :all
     else
       # can :read, :all
-      
+
       can :read, Page, public: true
       can :read, Location
       can :read, Project
       can :read, Member
-      
+
       #############################################################
       # What ALL logged in user can do
       #############################################################
@@ -25,18 +25,18 @@ class Ability
         can :read, Group
         can :read, Page
       end
-      
+
       #############################################################
       # Resources a default User can manage by his own
       #############################################################
       can :update, User do |u|
         u === user
       end
-      
+
       can :update, Member do |member|
         user.member === member
       end
-      
+
       can :update, TeamMembership do |tms|
         user.member === tms.member
       end
@@ -52,34 +52,35 @@ class Ability
         user.member.has_role?(:group_leader, group)
       end
       #############################################################
-      
+
       if user.has_role?(:member_manager)
         can :manage, Member
         can :manage, Group
         can :manage, GroupMembership
       end
-      
+
       if user.has_role?(:site_manager)
         can :manage, Page
         can :manage, PageFile
         can :manage, Guestbook
         can :manage, Location
       end
-      
+
       if user.has_role?(:project_manager)
         can :manage, Project
         can :manage, ProjectFile
         can :manage, Team
         can :manage, TeamMembership
+        can :manage, Event
       end
     end
-    
-    # The first argument to `can` is the action you are giving the user 
+
+    # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
     # here are :read, :create, :update and :destroy.
     #
-    # The second argument is the resource the user can perform the action on. 
+    # The second argument is the resource the user can perform the action on.
     # If you pass :all it will apply to every resource. Otherwise pass a Ruby
     # class of the resource.
     #
