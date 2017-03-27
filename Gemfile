a27740a1a9e4f186dc90a40824da6450f9e7867c
Florian Günther <mail@gee-f.de>
2017-03-27 13:39:25 +0200

    removed modernizer and fixed updating teams bug

--- Gemfile
+++ Gemfile
@@ -68,8 +68,8 @@ gem 'jbuilder'
 
 # platforms :ruby do
   gem 'unicorn' # Use unicorn as the app server
-  gem 'mysql2', '0.3.11'
-  gem 'activerecord-mysql-adapter'
+  gem 'mysql2'
+  gem 'activerecord-mysql2-adapter'
 # end
 
 # platforms :mswin, :mingw do
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,27 +2,27 @@
 class TeamsController < ApplicationController
   load_and_authorize_resource
   add_breadcrumb 'Projekte', :projects_path
-  
+
   def new
     @project = Project.find(params[:project_id])
     @team = @project.teams.new
   end
-  
+
   def create
     @project = Project.find(params[:project_id])
     @team = @project.teams.create(params[:team])
     redirect_to edit_project_team_path(@project, @team)
   end
-  
+
   def edit
     add_breadcrumb @team.project.title, edit_project_path(@team.project, anchor: 'teams')
     add_breadcrumb @team.name
-    
+
     @team_membership = TeamMembership.new
     @prev = @team.prev_in(@team.project.teams)
     @next = @team.next_in(@team.project.teams)
   end
-  
+
   def update
     if params[:move]
       target = Team.find(params[:team][:id].to_i)
@@ -31,31 +31,31 @@ class TeamsController < ApplicationController
         i.save
       end
     else
-    
-      if params[:files]
+
+      if params[:files] and params[:files] != ""
         @team.file = params[:files]
         @team.save
       end
-    
+
       if params[:team_memberships]
         # Mass-Update GroupMemberships
         @team.team_memberships.update params[:team_memberships].keys, params[:team_memberships].values
       end
-    
+
       @team.update_attributes params[:team]
-      
+
     end
-    
+
     redirect_to action: :edit
-    
+
   end
-  
+
   def destroy
     project = @team.project
-    
+
     @team.destroy
-    
+
     redirect_to edit_project_path(project, anchor: 'teams')
   end
-  
+
 end
